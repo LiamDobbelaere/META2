c1e6377de4c85716c93afd24b8d2ab635aeed741
Tom Dobbelaere <tom.dobbelaere@student.howest.be>
2018-05-04 20:25:45 +0200

    Mobile controls fail

---
+++ Assets/Scripts/PropagateControls.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PropagateControls : MonoBehaviour {
+    public enum Control { Accelerate, Decelerate, TurnLeft, TurnRight, PitchUp, PitchDown, TwistLeft, TwistRight};
+    public Control control;
+
+    private ShipController shipController;
+
+    private RectTransform rectTransform;
+
+    // Use this for initialization
+    void Start () {
+        shipController = GameObject.FindGameObjectWithTag("Player").GetComponent<ShipController>();
+        rectTransform = GetComponent<RectTransform>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            // Get movement of the finger since last frame
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+            Debug.Log("!");
+            rectTransform.anchoredPosition += touchDeltaPosition;
+        }
+    }
+}
--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -2,21 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class ShipController : MonoBehaviour {
     public Rigidbody rb;
     public float paramDivider = 30f;
 
-	// Use this for initialization
-	void Start () {
+    public float accelerationAxis = 0f;
+    public float turningAxis = 0f;
+    public float tiltAxis = 0f;
+    public float spinAxis = 0f;
+
+    private float accelerationAxisSmooth = 0f;
+    private float turningAxisSmooth = 0f;
+    private float tiltAxisSmooth = 0f;
+    private float spinAxisSmooth = 0f;
+
+    private float t = 0.1f;
+
+    // Use this for initialization
+    void Start () {
         rb = GetComponent<Rigidbody>();
     }
 
 	// Update is called once per frame
 	void Update () {
-        rb.AddRelativeForce(new Vector3(0, 0, Input.GetAxis("Vertical") * 50f));
+        accelerationAxis = Input.GetAxis("Vertical");
+        turningAxis = Input.GetAxis("Horizontal");
+        tiltAxis = Input.GetAxis("Tilt");
+        spinAxis = Input.GetAxis("Spin");
 
-        rb.AddRelativeTorque(new Vector3(Input.GetAxis("Tilt") * (0.3f / paramDivider), Input.GetAxis("Horizontal") * (0.6f / paramDivider), Input.GetAxis("Spin") * (0.2f / paramDivider)));
+        accelerationAxisSmooth = Mathf.Lerp(accelerationAxisSmooth, accelerationAxis, t);
+        turningAxisSmooth = Mathf.Lerp(turningAxisSmooth, turningAxis, t);
+        tiltAxisSmooth = Mathf.Lerp(tiltAxisSmooth, tiltAxis, t);
+        spinAxisSmooth = Mathf.Lerp(spinAxisSmooth, spinAxis, t);
+
+        rb.AddRelativeForce(new Vector3(0, 0, accelerationAxisSmooth * 50f));
+        rb.AddRelativeTorque(new Vector3(tiltAxisSmooth * (0.3f / paramDivider), 
+            turningAxisSmooth * (0.6f / paramDivider), spinAxisSmooth * (0.2f / paramDivider)));
 
         rb.velocity *= 0.95f;
         rb.angularVelocity *= 0.95f;
